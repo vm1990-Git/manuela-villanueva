2ecb002570e728d96ea93bc8bbea6de484cd1470
vm1990-Git <v.m.1990@hotmail.com>
2024-05-22 13:48:34 -0300

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Dra. Manuela Villanueva",
   description: "Professional website of neurologist  Dra. Manuela Villanueva",
+  icons: {
+    icon: ["favicon.ico?v=4"],
+    apple: ["apple-touch-icon.png?=4"],
+    shortcut: ["/apple-touch-icon.png"],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
